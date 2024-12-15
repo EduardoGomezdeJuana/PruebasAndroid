23f4ecfbc1b4159ce0deb92b96f4ff97dde1247f
eduardo-gomez-juana <eduardo.gomez43@educa.madrid.org>
2024-12-15 20:22:03 +0100

    Ejemplo indent nueva librería ActivityResult

--- app/src/main/java/com/egomezdj/pruebasandroid/firstapp/FirstAppActivity.kt
+++ app/src/main/java/com/egomezdj/pruebasandroid/firstapp/FirstAppActivity.kt
@@ -2,36 +2,40 @@ package com.egomezdj.pruebasandroid.firstapp
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
 import androidx.appcompat.widget.AppCompatEditText
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.activity.result.contract.ActivityResultContracts
 import com.egomezdj.pruebasandroid.R
 
 class FirstAppActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_first_app)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
         val btnStart = findViewById<AppCompatButton>(R.id.btnStart)
         val etName = findViewById<AppCompatEditText>(R.id.etName)
-        btnStart.setOnClickListener{
-            val name = etName.text.toString()
-            if (name.isNotEmpty()) {
-                // Crea intent para lanzar ResultActivity
-                val intent = Intent(this, ResultActivity::class.java)
-                // Paso de información name
-                intent.putExtra("EXTRA_NAME",name)
-                // Lanza la segunda actividad
-                startActivity(intent)
+        val etEdad = findViewById<AppCompatEditText>(R.id.etEdad)
+
+        // Registrar el ActivityResultLauncher
+        val launcher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) { result ->
+            if (result.resultCode == RESULT_OK) {
+                val data =result.data  // Obtenemos el intent con los datos
+                val name = data?.getStringExtra("nombre")
+                val age = data?.getIntExtra("edad", -1)
+                //textView.text = "Nombre: $name\nEdad: $age"
             }
+        }
+
+        // Botón para abrir la segunda actividad
+        btnStart.setOnClickListener {
+            val intent = Intent(this, ResultActivity::class.java)
+            intent.putExtra("nombre", etName)
+            intent.putExtra("edad", etEdad)
+            launcher.launch(intent)
         }
     }
 }
+
--- app/src/main/java/com/egomezdj/pruebasandroid/firstapp/ResultActivity.kt
+++ app/src/main/java/com/egomezdj/pruebasandroid/firstapp/ResultActivity.kt
@@ -2,7 +2,6 @@ package com.egomezdj.pruebasandroid.firstapp
 
 import android.os.Bundle
 import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -11,16 +10,23 @@ import com.egomezdj.pruebasandroid.R
 class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_result)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
         val tvResult = findViewById<TextView>(R.id.tvResult)
-        // Nos da valor de la clave EXTRA_NAME si no es vacio o vacio en caso contrario
-        val name:String = intent.extras?.getString("EXTRA_NAME").orEmpty()
-        tvResult.text = getString(R.string.respuesta, name)
+        val btnReturn = findViewById<TextView>(R.id.btnResult)
+
+        // Recibir los datos enviados desde FirstActivity
+        val name = intent.getStringExtra("nombre")
+        val age = intent.getIntExtra("edad", -1)
+//      tvResult.text = getString(R.string.respuesta, name, age)
+
+        // Devolver los datos a la primera actividad
+        btnReturn.setOnClickListener {
+            val resultIntent = Intent()
+            resultIntent.putExtra("nombre", name)
+            resultIntent.putExtra("edad", age)
+            setResult(Activity.RESULT_OK, resultIntent)
+            finish() // Cerrar ResultActivity
+        }
     }
 }
